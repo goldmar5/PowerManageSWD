3cd246fbaf403734c8b23d4bbe9f10f441afad9a
goldmar5 <goldmar5@users.noreply.github.com>
2014-03-04 11:17:53 +0200

    commented empty [TestMethod]

--- SWD.Starter/src/Demo.TestProject/UnitTest1.cs
+++ SWD.Starter/src/Demo.TestProject/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace Demo.TestProject
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+        //[TestMethod]
+        //public void TestMethod1()
+        //{
+        //}
     }
 }
